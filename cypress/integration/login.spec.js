c18d6d9a92d45c62077864fdb9a7db6c989700ea
SnaweNgorongoro <snawe2@gmail.com>
2022-03-14 23:16:03 +0100

    nadam se da cu u petak bolje uraditi

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+it('login test with valid credentials', () => {
+    cy.visit('/')
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__inner').type('snawe_2@yahoo.co.uk')
+    cy.get('[type="password"]').type('misica1304')
+    cy.get('.vs-u-text--left > .vs-c-btn').click()
+    cy.get('.vs-c-my-organization__header--add-new > .vs-c-my-organization__title').click()
+    cy.get('input').type('Organ')
+    cy.get('[name="next_btn"]').contains('Next').click()
+    cy.get('[name="next_btn"]').contains('Create').click()
+    cy.get('.vs-c-modal--features-button > .vs-c-btn').click()
+    cy.get('.vs-c-organization-boards__item--add-new').click()
+    cy.get('.vs-input-border > input').type('Organ-board')
+    cy.get('[name="next_btn"]').contains('Next').click()
+    cy.get('input[type=radio]').check('scrum_board', {force: true})
+    cy.get('[name="next_btn"]').click()
+    cy.get('[name="next_btn"]').click()
+    cy.get('[name="next_btn"]').contains('Finish').click()
+    
+    
+
+    
+    
+
+    
+
+
+})
